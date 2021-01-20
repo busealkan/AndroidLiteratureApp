2d2cc310c74004b63aa92d17410ec83e320c7ea3
Buse Alkan <buse_alkan10@hotmail.com>
2021-01-21 00:37:13 +0300

    json  değişti

--- app/src/main/java/com/h5190059/buse_alkan_final/utils/Constants.java
+++ app/src/main/java/com/h5190059/buse_alkan_final/utils/Constants.java
@@ -4,6 +4,4 @@ public class Constants {
     public  static String TASINAN_YAZAR_BASLIGI = "tiklananYazar";
 
     public  static String BASE_URL = "https://raw.githubusercontent.com/busealkan/h5190059busealkan/master/data/";
-
-
 }
