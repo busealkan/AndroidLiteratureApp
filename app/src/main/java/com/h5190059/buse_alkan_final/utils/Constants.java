8cf22b6c3b622cb24161467d0703885467c365b9
Buse Alkan <buse_alkan10@hotmail.com>
2021-01-21 00:29:51 +0300

    json url değişti

--- app/src/main/java/com/h5190059/buse_alkan_final/utils/Constants.java
+++ app/src/main/java/com/h5190059/buse_alkan_final/utils/Constants.java
@@ -3,7 +3,7 @@ package com.h5190059.buse_alkan_final.utils;
 public class Constants {
     public  static String TASINAN_YAZAR_BASLIGI = "tiklananYazar";
 
-    public  static String BASE_URL = "https://github.com/busealkan/h5190059busealkan/blob/master/data/";
+    public  static String BASE_URL = "https://raw.githubusercontent.com/busealkan/h5190059busealkan/master/data/";
 
 
 }
